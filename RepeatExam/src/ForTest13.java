5816afffb28baec09bfb62ed60e924a138462a15
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-14 18:42:30 +0900

    update

--- RepeatExam/src/ForTest13.java
+++ RepeatExam/src/ForTest13.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
 
 /*
+ * for문으로 
+ * 윤년을 구하라
  * 
- * 1~100의 범위에서 홀수만 더한다
- * 단 10번 더했다면 10번까지 더한 값을 출력한다
- * ============================
- * 1번째 누적값: 0 + 1 = 1
- * 2번째 누적값: 1 + 3 = 4
- * 3번째 누적값: 4 + 5 = 9
+ * 1~2020년까지
+ * ===========================
+ * 윤년
+ * 4
+ * 8
+ * .
+ * .
+ * .
+ * 2020
+ * 횟수 : 20
  *  
  *  
  * */
@@ -15,23 +20,36 @@ public class ForTest13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int cnt = 0;
 
-		int sum = 0;
-		int j = 1;
-		int cnt = 1;
-		//String str = ""
-
-		for (int i = 1; i <= 100; i++) {
-			System.out.print(cnt + "번째 누적값 :");
-			System.out.println(sum + " + " + j + " = " + (sum + j));
-			
-			if (j % 2 != 0) {
-				sum += j;
-				cnt++;
-				
-			}
-			j += 2;
-			if(cnt > 10) break;			
+		for(int i = 1600; i <= 2020; i++) {
+			if(i % 4 == 0) {                         // 4로 나눠지면 윤년
+				if(i % 100 == 0) {                   // 100으로 나눠지면 평년
+					if( i % 400 == 0) {              // 근데 또 400으로 나눠지면 윤년
+						System.out.println(i);
+						cnt++;
+					}
+				}else {
+					cnt++;
+					System.out.println(i);		
+				}
+			}			
 		}
+		System.out.println("횟수 : " + cnt);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
